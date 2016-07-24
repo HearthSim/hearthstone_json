20308c2040ab1d1d7e4772be0bbd4b02007fbfe2
Alex Hanna <tinnvec@gmail.com>
2016-07-24 13:34:17 -0500

    Add very basic setup for httparty

--- lib/hearthstone_json.rb
+++ lib/hearthstone_json.rb
@@ -1,2 +1,18 @@
+require 'httparty'
+
 class HearthstoneJSON
+    include HTTParty
+
+    API_URL_BASE = 'https://api.hearthstonejson.com'.freeze
+    API_VER = 'v1'.freeze
+
+    base_uri "#{API_URL_BASE}/#{API_VER}"
+
+    def initialize(locale = 'enUS', data_ver = 'latest')
+        @full_uri = "#{base_uri}/#{data_ver}/#{locale}"
+    end
+
+    private
+
+    attr_accessor :locale
 end
